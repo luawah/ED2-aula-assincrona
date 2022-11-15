24670086a87784527d79d8990f8faee6711b6358
Pedro Palma <pedrolucaspalma@gmail.com>
2022-11-15 20:42:35 -0300

    add questão 2

---
+++ 2.js
@@ -0,0 +1,15 @@
+const nums = [1, 3, 7, 21, 32, 44, 92, 11];
+
+let target = 28;
+
+const returnIndicies = (target, nums) => {
+	for (let i = 0; i < nums.length; i++) {
+		let val1 = nums[i];
+		for (let j = i; j < nums.length; j++) {
+			let val2 = nums[j];
+			if (val1 + val2 === target) return [i, j];
+		}
+	}
+};
+
+console.log(returnIndicies(target, nums));
